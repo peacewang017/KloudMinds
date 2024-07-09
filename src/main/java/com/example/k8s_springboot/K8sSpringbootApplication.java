9568b19fc73cca6a865bc55ecdd6bb3d641b7a6f
lrp129 <2966631798@qq.com>
2024-07-09 23:27:24 +0800

    1

---
+++ src/main/java/com/example/k8s_springboot/K8sSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8s_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/k8s_springboot/K8sSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8s_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
